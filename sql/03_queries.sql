dd1c2f9a0c9c4789eab5a4fcf1a8e6b771176909
bigsas777 <luca.zorzi77@gmail.com>
2026-06-07 16:12:50 +0200

    Update modify queries section

---
+++ sql/03_queries.sql
@@ -0,0 +1,79 @@
+-- Determinare quale insegnamento compare più frequentemente nei piani di studio
+SELECT corso.nome, COUNT(*) AS frequenza
+FROM piano_di_studio
+JOIN corso ON piano_di_studio.codice_corso = corso.codice
+GROUP BY corso.codice, corso.nome
+ORDER BY frequenza DESC
+LIMIT 1;
+
+
+-- Dato nome e cognome di un professore, un anno accademico e un periodo didattico restituire il suo calendario settimanale delle lezioni (insegnamento, giorno, fascia oraria, aula)
+SELECT corso.nome, lezione.giorno, lezione.fascia_oraria, lezione.aula
+FROM lezione
+JOIN corso ON lezione.codice_corso = corso.codice
+JOIN insegnamento_edizione ON lezione.codice_corso = insegnamento_edizione.codice_corso 
+    AND lezione.anno_accademico = insegnamento_edizione.anno_accademico 
+    AND lezione.periodo = insegnamento_edizione.periodo
+JOIN docente ON insegnamento_edizione.cf_docente = docente.cf
+WHERE docente.nome = 'Maria' 
+    AND docente.cognome = 'Gozzi' 
+    AND lezione.anno_accademico = '2024/2025' 
+    AND lezione.periodo = '2';
+
+
+-- Dato un insegnamento, calcolare la percentuale di prove superate rispetto al totale dei tentativi registrati
+WITH tentativi_totali (codice_corso, totali) AS (
+    SELECT codice_corso, COUNT(*)
+    FROM esame
+    GROUP BY codice_corso
+), 
+tentativi_superati (codice_corso, superati) AS (
+    SELECT codice_corso, COUNT(*) AS superati
+    FROM esame
+    WHERE punteggio >= 18
+    GROUP BY codice_corso
+)
+SELECT corso.nome, (tentativi_superati.superati * 100.0 / tentativi_totali.totali) AS percentuale_superamento
+FROM tentativi_totali
+JOIN tentativi_superati ON tentativi_totali.codice_corso = tentativi_superati.codice_corso
+JOIN corso ON tentativi_totali.codice_corso = corso.codice
+WHERE corso.nome = 'Logica matematica';
+
+
+-- Dato un insegnamento, determinare la media dei tentativi effettuati dagli studenti che hanno effettivamente superato la prova
+WITH tentativi_per_passare (matricola, num_tentativi) AS (
+    SELECT esame.matricola, COUNT(*)
+    FROM esame
+    JOIN corso ON esame.codice_corso = corso.codice
+    WHERE corso.nome = 'Teoria dei segnali'
+    GROUP BY esame.matricola
+    HAVING MAX(esame.punteggio) >= 18
+)
+SELECT AVG(num_tentativi) AS media_tentativi
+FROM tentativi_per_passare;
+
+
+-- Dato uno studente, si vuole gestire l'eliminazione di tutti i suoi dati in seguito ad una rinuncia agli studi
+BEGIN;
+-- Necessario per ON DELETE RESTRICT di matricola su esame
+DELETE FROM esame
+WHERE matricola = 15;
+
+-- Rimozione del piano di studi gestita in automatico
+DELETE FROM studente
+WHERE matricola = 15;
+COMMIT;
+
+
+-- Dato un professore e l'anno accademico, si vuole gestire la sostituzione delle sue cattedre
+UPDATE insegnamento_edizione
+SET cf_docente = 'ZCCNTL28S02G280O'
+WHERE insegnamento_edizione.cf_docente = 'PTRFBA87S13G935U'
+  AND insegnamento_edizione.anno_accademico = '2025/2026'
+  AND EXISTS (
+      -- Verifica abilitazione nuovo docente
+      SELECT 1 
+      FROM abilitazione_docente_corso
+      WHERE abilitazione_docente_corso.cf_docente = 'ZCCNTL28S02G280O' 
+        AND abilitazione_docente_corso.codice_corso = insegnamento_edizione.codice_corso
+  );

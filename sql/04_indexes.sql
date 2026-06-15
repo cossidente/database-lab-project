360e60ed5bdfba0cec77adf1db22a3f690bf4d88
Carlo Malocco <163123963+maloccocarlo@users.noreply.github.com>
2026-06-15 23:08:57 +0200

    Aggiunti indici secondari per query e foreign key

---
+++ sql/04_indexes.sql
@@ -0,0 +1,8 @@
+-- Indici per i filtri di ricerca (Query)
+CREATE INDEX idx_docente_cognome_nome ON docente (cognome, nome);
+CREATE INDEX idx_insegnamento_nome ON insegnamento (nome);
+CREATE INDEX idx_insegnamento_edizione_ricerca ON insegnamento_edizione (codice_insegnamento, anno_accademico, periodo);
+
+-- Indici per le seconde colonne di PK composite (Query + FK)
+CREATE INDEX idx_piano_di_studio_insegnamento ON piano_di_studio (codice_insegnamento);
+CREATE INDEX idx_esame_insegnamento ON esame (codice_insegnamento);
